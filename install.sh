205f83375d1fc0f0cc96f62a864adf8bbad50df2
Nick Girga <nickgirga@gmail.com>
2020-12-04 14:49:31 -0800

    remove sudo from install.sh and uninstall.sh; check for sym link

--- install.sh
+++ install.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root or with sudo"
+    exit
+fi
+
 # to-do: install needed dependencies before installing name-grabber
 
 # create resource folder in /usr/share
-sudo mkdir -p /usr/share/name-grabber
+mkdir -p /usr/share/name-grabber
 
 # copy resources into newly created resource folder
-sudo cp -i ./name-grabber /usr/share/name-grabber/name-grabber
-sudo cp -i ./main.glade /usr/share/name-grabber/main.glade
+cp -i ./name-grabber /usr/share/name-grabber/name-grabber
+cp -i ./main.glade /usr/share/name-grabber/main.glade
 
 # link name-grabber executable to the /usr/bin folder
-sudo ln -s /usr/share/name-grabber/name-grabber /usr/bin/name-grabber
+if [ ! -f /usr/bin/name-grabber ]; then
+    ln -s /usr/share/name-grabber/name-grabber /usr/bin/name-grabber
+fi
 
 # to-do: create launcher icon
 
--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root or with sudo"
+    exit
+fi
+
 # remove resource folder and contents in /usr/share
-sudo rm -rf /usr/share/name-grabber/
+rm -rf /usr/share/name-grabber/
 
 # remove link to executable in the /usr/bin folder
-sudo rm -f /usr/bin/name-grabber
+rm -f /usr/bin/name-grabber
 
 # to-do: remove launcher icon
 
