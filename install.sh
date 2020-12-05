c68609789c1132216fe5d8e0c1429a94f319899b
Nick Girga <nickgirga@gmail.com>
2020-12-04 22:28:32 -0800

    copy launcher icon to res folder and link to it for consistency

--- install.sh
+++ install.sh
@@ -15,14 +15,17 @@ mkdir -p /usr/share/name-grabber
 cp -i ./name-grabber /usr/share/name-grabber/name-grabber
 cp -i ./main.glade /usr/share/name-grabber/main.glade
 cp -ir ./res /usr/share/name-grabber/res
+cp -i ./name-grabber.desktop /usr/share/name-grabber/name-grabber.desktop
 
 # link name-grabber executable to the /usr/bin folder
 if [ ! -f /usr/bin/name-grabber ]; then
     ln -s /usr/share/name-grabber/name-grabber /usr/bin/name-grabber
 fi
 
-# copy launcher icon to /usr/share/applications
-cp -i ./name-grabber.desktop /usr/share/applications/name-grabber.desktop
+# link launcher icon to /usr/share/applications
+if [ ! -f /usr/share/applications/name-grabber.desktop ]; then
+    ln -s /usr/share/name-grabber/name-grabber.desktop /usr/share/applications/name-grabber.desktop
+fi
 
 #report to user that installation is complete
 echo Installation complete!
